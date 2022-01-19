8157ccdb540f728064e443004992f7a96a4c767e
Orlando Curieles <orlando.curieles@ingeint.com>
2022-01-19 17:44:16 -0400

    Added 2pack

--- src/com/ingeint/event/ValidateBankAccount.java
+++ src/com/ingeint/event/ValidateBankAccount.java
@@ -11,20 +11,18 @@ public class ValidateBankAccount extends CustomEventHandler {
 	@Override
 	protected void doHandleEvent() {
 		MBPBankAccount baccount = (MBPBankAccount) getPO();
-		
+
 		if (!baccount.getC_BPartner().isEmployee())
 			return;
-		
+
 		int MaxDigit = MSysConfig.getIntValue("MaxDigitBankAccount", 0, baccount.getAD_Client_ID());
-		
+
 		if (MaxDigit == 0)
 			return;
-		
+
 		if (!(MaxDigit == baccount.getA_Name().length()))
-			throw new AdempiereException("@NotMaxDigitAccount@"+MaxDigit);
-			
-		
-		
+			throw new AdempiereException("@NotMaxDigitAccount@" + MaxDigit);
+
 	}
 
 }
